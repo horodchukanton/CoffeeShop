8b04563b252876c398fa0803050ee82ce7173e94
Andrew <0685870650a@gmail.com>
2017-10-29 17:17:43 +0200

    Add webinterface to lib.

--- lib/CoffeeShop/Webinterface.pm
+++ lib/CoffeeShop/Webinterface.pm
@@ -4,15 +4,50 @@ use warnings FATAL => 'all';
 
 
 use Dancer2;
+use CoffeeShop::DB qw/resultset/;
+
+set 'appdir' => '/usr/bukinist/lib/CoffeeShop/Webinterface';
+set 'views'  => config->{appdir} . "/views/";
+set 'layout' => 'main';
+set 'template' => 'TemplateToolkit';
 
 get '/' => sub {
-#    template 'index';
-    return "Hello world";
+    session('user')
+      or redirect('/login');
+    
+    template index => {};
   };
 
-get '/auth' => sub {
-    #    template 'index';
-    return "Auth page";
+get '/login' => sub {
+#    use Data::Dumper;
+#    return Dumper(config);
+    my %TEMPLATE_PARAMS = ();
+    
+    if(query_parameters->{'failed'}){
+      $TEMPLATE_PARAMS{FAILED} = 'Login not found';
+      
+    }
+    
+    template login => \%TEMPLATE_PARAMS;
+  };
+
+post '/login' => sub {
+    
+    use Dancer2::Serializer::Dumper;
+    Dancer2::Serializer::Dumper->serialize(body_parameters);
+    my $username  = body_parameters->get('username');
+    my $redir_url = body_parameters->get('redirect_url') || '/login';
+    
+    my $Admin = resultset('Administrator');
+    my $admin = $Admin->find({id => $username});
+    
+    if($admin){
+      session user => $username;
+      redirect $redir_url;
+    }
+    else{
+      redirect '/login?failed=1';
+    }
   };
 
 1;
